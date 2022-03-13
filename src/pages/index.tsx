8ee6e7b6b609462bbf0cb0ba6d2e9f260879845c
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-03-12 23:43:22 -0300

    rendered component Profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import { Navigation } from "../components/Navigation";
+import { Profile } from "../components/Profile";
 
 function Home() {
   return (
@@ -7,28 +8,9 @@ function Home() {
       <Head>
         <title>ronaldtomaz.dev</title>
       </Head>
+
       <div className="w-full mx-11 flex flex-col md:mx-auto md:w-80">
-        <div className="flex flex-col items-center">
-          <div className="px-1 py-1 border-4 border-cyan-400 rounded-full">
-            <img
-              src="https://github.com/ronaldprofile.png"
-              alt="Ronald Tomaz"
-              className="
-              w-24
-              h-24
-              cursor-pointer
-              rounded-full
-
-              md:w-52
-              md:h-52
-              "
-            />
-          </div>
-
-          <strong className="text-2xl mt-4 font-sans text-white">
-            Ronald Tomaz
-          </strong>
-        </div>
+        <Profile />
 
         <Navigation />
 
