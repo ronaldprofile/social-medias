2dcfc650ffb5019e757536a8df897cf4ce4ca778
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-08-06 15:48:11 -0300

    added label developer

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -10,14 +10,19 @@ export function Avatar({ avatar_url, name }: AvatarProps) {
     <motion.div
       initial={{ opacity: 0, scale: 0.8 }}
       animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1 }}
-      whileHover={{ scale: 1.05 }}
-      className="p-1 border-4 border-cyan-400 rounded-full"
+      transition={{ duration: 1, ease: ['easeIn'] }}
+      className="p-2 relative border-4 border-cyan-400 rounded-full"
     >
+      <motion.div className="w-full absolute -bottom-1 left-0 text-center">
+        <span className="px-4 py-[7px] uppercase text-xs bg-slate-600 text-cyan-400 font-medium rounded-[5px]">
+          developer
+        </span>
+      </motion.div>
+
       <motion.img
         src={avatar_url}
         alt={name}
-        className="w-24 h-24 cursor-pointer rounded-full md:w-52 md:h-52"
+        className="w-24 h-24 cursor-pointer rounded-full md:w-40 md:h-40"
       />
     </motion.div>
   );
